0d457c0cfba8b299ba6c30effa6b890f0cfa1d07
Zahraa-Jawad <zhrjwd27@gmail.com>
2024-10-09 23:38:42 +0300

    init

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  <input type="text" v-model="taskname" placeholder="enter task">    
+  <button @click="addtask">add task</button>
+
+  <h4 v-for="task in tasks">{{ task }}</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  const taskname = ref("")
+
+  const tasks = ref([])
+
+  const addtask = () => {
+    tasks.value.push(taskname.value)
+  }
+
+</script>
